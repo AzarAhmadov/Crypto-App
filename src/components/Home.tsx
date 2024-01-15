b165470a3e28b09f21eca7f941c6a6dde9b50443
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2024-01-15 19:37:00 +0400

    Crypto App using RTK query

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,58 +1,106 @@
-import React, { useEffect } from 'react';
-import { useGetCryptoMutation } from '../services/cryptoApi';
+import React, { useEffect, useState } from 'react';
+import { useGetAllCryptoQuery, useGetCryptosMutation } from '../services/cryptoApi';
 import Table from 'react-bootstrap/Table';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
 import { CoinType } from '../types/CoinsType';
+import { Items } from './Items';
+import Button from 'react-bootstrap/Button';
+import Search from './Search';
+import Spinner from 'react-bootstrap/Spinner';
 
 const Home: React.FC = () => {
-    const [getCrypto, { data: cryptos }] = useGetCryptoMutation();
+    const [getCrypto, { data: cryptos }] = useGetCryptosMutation();
+    const { data, isLoading } = useGetAllCryptoQuery()
+    const [perPage, setPerpage] = useState<string>('10')
+    const [name, setName] = useState<string>('')
+    const [page, setPage] = useState(1)
+    const pages = ["5", "10", "15", "20", "25", "30"];
+
+    const filteredCoins = data?.filter((coin) =>
+        coin.name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    const handlePage = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setPerpage(e.target.value);
+    };
 
     useEffect(() => {
-        const fetchData = async () => {
-            await getCrypto(1);
-        };
-        fetchData();
-    }, []);
+        fetchData()
+    }, [page, perPage]);
+
+    const fetchData = async () => {
+        await getCrypto({ page, perPage });
+    }
 
     return (
         <main>
-            <Table striped bordered hover>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Coin</th>
-                        <th>Price</th>
-                        <th>24h</th>
-                        <th>Volume</th>
-                        <th>Market Cap</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {cryptos?.length ? (
-                        cryptos.map((coin: CoinType, idx: number) => (
-                            <tr key={idx}>
-                                <td>{coin.market_cap_rank}</td>
-                                <td>
-                                    <Link to={`/detail/${coin.id}`} key={coin.id} className="img-info">
-                                        <img src={coin?.image} alt={coin?.name} />
-                                        <p>{coin?.symbol.toUpperCase()}</p>
-                                    </Link>
-                                </td>
-                                <td>${coin.current_price.toLocaleString()}</td>
-                                <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-                                <td>${coin.total_volume.toLocaleString()}</td>
-                                <td>${coin.market_cap.toLocaleString()}</td>
+            <Search name={name} setName={setName} />
+            {isLoading ? (
+                <Spinner className='spinner' animation="grow" variant="light" />
+            ) : (
+                <div className='table-area'>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Coin</th>
+                                <th>Price</th>
+                                <th>24h</th>
+                                <th>Volume</th>
+                                <th>Market Cap</th>
                             </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <td colSpan="6">No data available</td>
-                        </tr>
-                    )}
+                        </thead>
+                        <tbody>
+                            {!name ? (
+                                cryptos && cryptos.length > 0 ? (
+                                    cryptos.map((coin: CoinType, idx: number) => (
+                                        <Items coin={coin} key={idx} />
+                                    ))
+                                ) : (
+                                    <td className='text-center p-4 text-danger fw-bold fs-5' colSpan={6}>No data available</td>
+                                )
+                            ) : (
+                                filteredCoins && filteredCoins.length > 0 ? (
+                                    filteredCoins.map((coin: CoinType, idx: number) => (
+                                        <Items key={idx} coin={coin} />
+                                    ))
+                                ) : (
+                                    <td className='text-center p-4 text-danger fw-bold fs-5' colSpan={6}>No data available</td>
+                                )
+                            )}
+                        </tbody>
+                    </Table>
+                </div>
+            )}
 
-                </tbody>
-            </Table>
+            {cryptos && (
+                <div className="flex-area">
+                    <div className="button-control">
+                        <Button
+                            disabled={page === 1}
+                            onClick={() => setPage((page) => page - 1)}
+                            variant="outline-light"
+                        >
+                            Prev
+                        </Button>
+                        <span className="fs-5 color-red"> {page} </span>
+                        <Button
+                            onClick={() => setPage((page) => page + 1)}
+                            variant="outline-light"
+                        >
+                            Next
+                        </Button>
+                    </div>
+
+                    <div>
+                        <span>Per Page: </span>
+                        <select value={perPage} onChange={handlePage}>
+                            {pages.map((page, index) => (
+                                <option key={index}>{page}</option>
+                            ))}
+                        </select>
+                    </div>
+                </div>
+            )}
         </main>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import App from './App.tsx'
 import './assets/css/App.css'
 import { store } from './app/store/Store.tsx';
 import { Provider } from 'react-redux';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
